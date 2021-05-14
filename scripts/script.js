1625d464cf22432e76437502d4e6ef76c43a0b8a
theting641 <theting641@gmail.com>
2021-05-14 00:36:23 -0700

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const setState = router.setState;
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
